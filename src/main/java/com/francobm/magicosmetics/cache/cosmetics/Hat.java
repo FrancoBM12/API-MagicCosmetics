f9c9eaf9c20f06d4dfa033d761ba1bb9777e18b6
BuildTools <unconfigured@null.spigotmc.org>
2023-06-06 16:34:48 -0500

    feat: the Hat class has been added so that certain properties of the hat can be accessed through the API.

---
+++ src/main/java/com/francobm/magicosmetics/cache/cosmetics/Hat.java
@@ -0,0 +1,15 @@
+package com.francobm.magicosmetics.cache.cosmetics;
+
+import com.francobm.magicosmetics.api.Cosmetic;
+import com.francobm.magicosmetics.utils.MCNotInstalledException;
+
+public class Hat extends Cosmetic {
+
+    public boolean isOverlaps() {
+        throw new MCNotInstalledException();
+    }
+
+    public double getOffSetY() {
+        throw new MCNotInstalledException();
+    }
+}
